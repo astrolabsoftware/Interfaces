e78e85a970309c12210454884ffc963115aaf9a9
Arnault <arnault@lal.in2p3.fr>
2018-06-05 09:37:11 +0200

    Fix Mul.c

--- Mul.c
+++ Mul.c
@@ -4,5 +4,6 @@ double mymultiply(double x, double y) {
 }
 
 void myarray(double array[], int arraylen) {
-  for (int i=0; i < arraylen; i++) { array[i] *= 2; }
+  int i = 0;
+  for (i=0; i < arraylen; i++) { array[i] *= 2; }
 }
